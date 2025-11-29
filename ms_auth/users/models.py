79919c4876209a609053642d279c070236f977bf
Cokke <jorgerandomgamer@gmail.com>
2025-11-29 19:18:26 -0300

    first commit - ms-auth

---
+++ ms_auth/users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    ROLES = (
+        ('ADMIN', 'Administrador'),
+        ('GESTOR', 'Controla tienda'),
+        ('CLIENTE', 'Usuario'),
+    )
+    
+    email = models.EmailField(unique=True) 
+    role = models.CharField(max_length=15, choices=ROLES, default='CLIENTE')
+    
+    USERNAME_FIELD = 'username' 
+    REQUIRED_FIELDS = ['email', 'first_name'] 
+    
+    def __str__(self):
+        return self.email
