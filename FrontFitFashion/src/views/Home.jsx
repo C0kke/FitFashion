b746b7b1599bdef26893cb9ee49d1857ea9e8e71
Cokke <jorgerandomgamer@gmail.com>
2025-11-29 19:31:14 -0300

    first commit front

---
+++ FrontFitFashion/src/views/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Fit Fashion</h1>
+            <p>Vistete a la moda y diseña outfits</p>
+        </div>
+    );
+};
+
+export default Home;
