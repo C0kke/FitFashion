c2e418d23b6d1db81b884e575c1852746a8a4ee6
Cokke <jorgerandomgamer@gmail.com>
2025-12-02 13:34:38 -0300

    Estilos base

--- FrontFitFashion/src/components/Navbar.jsx
+++ FrontFitFashion/src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import UserIcon from "../assets/user.svg";
 import "./styles/Navbar.css";
+import axios from 'axios';
+
+const BackURL = import.meta.env.VITE_BACK_URL;
 
 const Navbar = () => {
     const user = localStorage.getItem("user");
@@ -20,6 +23,23 @@ const Navbar = () => {
         window.location.href = "/";
     };
 
+    const handleLogout = async () => {
+        const token = localStorage.getItem("user");
+        if (token) {
+            try {
+                await axios.post(`${BackURL}/auth/token/logout/`, {}, {
+                    headers: {
+                        Authorization: `Token ${token}`
+                    }
+                });
+            } catch (error) {
+                console.error("Logout failed", error);
+            }
+            localStorage.removeItem("user");
+            window.location.reload();
+        }
+    };
+
     return (
         <div className="navbar">
             <h1 onClick={navigateToHome}>FitFashion</h1>
@@ -28,6 +48,7 @@ const Navbar = () => {
                 <button onClick={navigateToProfile}>
                     <img src={UserIcon} alt="User Icon" className="userIcon" />
                 </button>
+                {user && <button onClick={handleLogout}>Logout</button>}
             </div>
         </div>
     );
